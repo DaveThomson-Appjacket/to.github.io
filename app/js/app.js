5385feac79c7fe6a5c09f089e6d2f70756dfc7fd
DaveThomson-Appjacket <45829844+DaveThomson-Appjacket@users.noreply.github.com>
2019-04-08 18:15:49 -0400

    Update app.js

--- app/js/app.js
+++ app/js/app.js
@@ -90,7 +90,6 @@ function set_sections(data){
           if(counter%4 == 0){
             var newCanvas = $('<canvas/>',{'class':'glass', 'color':'white','alpha':.35}).width($(current_row).width()).height($(current_row).height());
             $(current_row).append(newCanvas);
-          $(contact_card).addClass(
         } 
         $(span_paragraphs).append(current_row);
         
