88a6c6d5dc67e46f3f67fcac39b2c23947a212fd
DaveThomson-Appjacket <45829844+DaveThomson-Appjacket@users.noreply.github.com>
2019-04-08 11:18:13 -0400

    Create app.js

---
+++ app/js/app.js
@@ -0,0 +1,14 @@
+function get_json(){
+  $.get("./app/json/app.json", function(data){
+  alert(JSON.stringify(data));
+  });
+}
+function get_title(){
+}
+function get_logo(){
+}
+function get_sections(){
+}
+$(document).ready(function(){
+  get_json();
+});
