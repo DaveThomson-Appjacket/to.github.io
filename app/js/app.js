1e2f1a49bc2f421fcaef2a79be3d5df9d593736f
DaveThomson-Appjacket <45829844+DaveThomson-Appjacket@users.noreply.github.com>
2019-04-08 15:09:17 -0400

    Update app.js

--- app/js/app.js
+++ app/js/app.js
@@ -26,6 +26,7 @@ function set_logo(data){
   var half_len_nav = Math.floor(len_nav/2);
   var image = $("<img/>");
   $(image).attr("src",data["site-logo"]);
+  $(image).addClass("site-logo");
   $(".nav").children().eq([half_len_nav - 1]).append(image);
 }
 function get_sections(){
